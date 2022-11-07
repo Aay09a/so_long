089680ec6df3c14ecb62bc9b8366bb9a4bff74d8
Aay09a <ayooba5675@outlook.com>
2022-11-07 23:34:00 +0400

    second

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ahussein <ahussein@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 19:15:14 by ahussein          #+#    #+#             */
-/*   Updated: 2022/11/07 23:32:14 by ahussein         ###   ########.fr       */
+/*   Updated: 2022/11/07 23:33:38 by ahussein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
-
 
 enum	e_keys
 {
